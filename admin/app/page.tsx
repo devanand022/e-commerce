e040e0986acf6fe5f80ff869fc2b9bb3895832d3
devanand022 <ananddeva578@gmail.com>
2025-10-28 13:32:14 +0530

    finished the theming

--- admin/app/page.tsx
+++ admin/app/page.tsx
@@ -1,13 +1,3 @@
-import { Button } from "@/components/ui/button";
-import { CirclePlus } from "lucide-react";
-
 export default function Home() {
-  return (
-    <div className="h-screen flex items-center justify-center">
-      <Button variant="outline" size="lg">
-        <CirclePlus />
-        Click Me
-      </Button>
-    </div>
-  );
+  return <div className=""></div>;
 }
